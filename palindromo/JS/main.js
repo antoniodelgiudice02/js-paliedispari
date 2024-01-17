4ecc41ad5bc44c4c104a1c0bdf54e1ff738841a2
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-17 16:41:09 +0100

    add: pari e dispari prompt

---
+++ palindromo/JS/main.js
@@ -0,0 +1,39 @@
+/*const word = prompt('scrivi una parola')
+console.log(word)
+
+const printedWord = document.getElementById('printed-word')
+const printedReverse = document.getElementById('printed-reverse')
+
+let i = 0
+console.log(i)
+let x = word.length
+console.log(x)
+
+while (i < word.length - 1) {
+    i++
+    console.log(word[i])
+    printedWord.innerText = `${word[i]}`
+
+}
+
+while (x > 0) {
+    x--
+    console.log(word[x])
+    printedReverse.innerText = `${word[x]}`
+
+}
+
+if(i === x){
+    console.log('è palindroma')
+} else{
+    console.log('non è palindroma')
+}*/
+
+
+
+
+
+
+
+
+
---
+++ pari e dispari/JS/main.js
@@ -0,0 +1,2 @@
+const scelta = prompt('Scegli pari o dispari')
+const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'))
