215d767d43ccdca21d23ed0b515e45442d929e99
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-17 16:56:03 +0100

    add: pari e dispari: function numero computer

--- pari e dispari/JS/main.js
+++ pari e dispari/JS/main.js
@@ -1,2 +1,15 @@
+
+//  NUMERO UTENTE 
+
 const scelta = prompt('Scegli pari o dispari')
 const numeroUtente = parseInt(prompt('Scegli un numero da 1 a 5'))
+
+// NUMERO COMPUTER
+
+function getRandomNumber(min, max) {
+    const randomNumber = Math.floor(Math.random() * (max - min - 1) + min);
+    return randomNumber;
+}
+
+const numeroComputer = getRandomNumber(1, 5);
+console.log(numeroComputer)
