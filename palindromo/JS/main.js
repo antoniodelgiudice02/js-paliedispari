7697546655b5fcea2889ca8b1ab0cac0198d220b
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-17 22:25:27 +0100

    PALINDROMI

--- palindromo/JS/main.js
+++ palindromo/JS/main.js
@@ -6,18 +6,24 @@ const printedReverse = document.getElementById('printed-reverse')
 
 // scrivo la parola al contrario con una funzione
 function ReverseString(word) {
-	return word.split('').reverse().join('')
+    return word.split('').reverse().join('')
 }
 
 const reverseWord = ReverseString(word)
 console.log(ReverseString(word))
 
-// trasformo le stringhe in array
 
-const wordArray = word.split('')
-const reverseArray = reverseWord.split('')
-console.log(wordArray)
-console.log(reverseArray)
+function palindromia(word1, word2) {
+    
+    if (word1 === word2) {
+        console.log('la parola è palindroma')
+    } else {
+        console.log('la parola non è palindroma')
+    }
+
+}
+
+palindromia(word, reverseWord)
 
 printedWord.innerText = word
 printedReverse.innerText = (reverseWord)
