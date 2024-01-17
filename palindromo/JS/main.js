337dea15cec2fbc778e4389bc924aa4a8f6e953c
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-17 21:33:26 +0100

    add: array

--- palindromo/JS/main.js
+++ palindromo/JS/main.js
@@ -4,15 +4,20 @@ console.log(word)
 const printedWord = document.getElementById('printed-word')
 const printedReverse = document.getElementById('printed-reverse')
 
-// Function to reverse string
+// scrivo la parola al contrario con una funzione
 function ReverseString(word) {
 	return word.split('').reverse().join('')
 }
 
-// Function call
 const reverseWord = ReverseString(word)
 console.log(ReverseString(word))
 
+// trasformo le stringhe in array
+
+const wordArray = word.split('')
+const reverseArray = reverseWord.split('')
+console.log(wordArray)
+console.log(reverseArray)
 
 printedWord.innerText = word
 printedReverse.innerText = (reverseWord)
