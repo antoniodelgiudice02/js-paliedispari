558b5e988abe690a1b932138da56dae0f12e2867
antoniodelgiudice02 <antodelgiu02@gmail.com>
2024-01-17 21:24:01 +0100

    add: palindromo function reverse

--- palindromo/JS/main.js
+++ palindromo/JS/main.js
@@ -1,38 +1,21 @@
-/*const word = prompt('scrivi una parola')
+const word = prompt('scrivi una parola')
 console.log(word)
 
 const printedWord = document.getElementById('printed-word')
 const printedReverse = document.getElementById('printed-reverse')
 
-let i = 0
-console.log(i)
-let x = word.length
-console.log(x)
-
-while (i < word.length - 1) {
-    i++
-    console.log(word[i])
-    printedWord.innerText = `${word[i]}`
-
+// Function to reverse string
+function ReverseString(word) {
+	return word.split('').reverse().join('')
 }
 
-while (x > 0) {
-    x--
-    console.log(word[x])
-    printedReverse.innerText = `${word[x]}`
-
-}
-
-if(i === x){
-    console.log('è palindroma')
-} else{
-    console.log('non è palindroma')
-}*/
+// Function call
+const reverseWord = ReverseString(word)
+console.log(ReverseString(word))
 
 
-
-
-
+printedWord.innerText = word
+printedReverse.innerText = (reverseWord)
 
 
 
